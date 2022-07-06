432c2900bd0a56755d0f29747c3d3aa9d2ea8432
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-06 14:19:27 +0430

    lightbox section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const tabMenu = document.querySelector("#tab-btn").children;
+const items = document.querySelector(".portfolio-gallery").children;
 
 for (let i = 0; i < tabMenu.length; i++) {
     tabMenu[i].addEventListener("click", function () {
@@ -6,5 +7,24 @@ for (let i = 0; i < tabMenu.length; i++) {
             tabMenu[j].classList.remove("active");
         }
         this.classList.add("active");
+        const target = this.getAttribute("data-target");
+
+        for (let k = 0; k < items.length; k++) {
+            items[k].style.display = "none";
+            if (target == items[k].getAttribute("data-id")) {
+                items[k].style.display = "block";
+            }
+            if (target == "all") {
+                items[k].style.display = "block";
+            }
+        }
     });
 }
+
+// lightbox
+
+const closeLightbox = document.querySelector(".close-lightbox");
+
+closeLightbox.addEventListener("click", function () {
+    document.querySelector(".lightbox").style.display = "none";
+});
